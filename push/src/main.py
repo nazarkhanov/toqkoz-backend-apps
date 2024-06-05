8f2484ceee4c9cb8a921b05f22608b7b2a8559a2
Alisher Nazarkhanov <alisher.nazarkhanov@aviata.me>
2024-06-05 06:02:42 +0500

    push updates

--- push/src/main.py
+++ push/src/main.py
@@ -1,3 +1,4 @@
+import json
 from uuid import UUID
 from fastapi import FastAPI, Response
 from firebase_admin import initialize_app, credentials, messaging
@@ -9,11 +10,30 @@ TITLE = 'PUSH'
 DESCRIPTION = 'The service is designed to send push notifications'
 
 
-app = FastAPI(title=TITLE, description=DESCRIPTION, root_path='/android')
+app = FastAPI(title=TITLE, description=DESCRIPTION)
 
 
-user_firebase_token = {}
-cred = credentials.Certificate('creds.json')
+class LocaleStorage:
+    def __init__(self) -> None:
+        self.values = {}
+
+        with open('src/storage.json', 'r+') as f:
+            try:
+                self.values = json.load(f)
+            except:
+                pass
+    
+    def read(self, key):
+        return self.values.get(key)
+
+    def write(self, key, value):
+        self.values[key] = value
+        with open('src/storage.json', 'w+') as f:
+            f.write(json.dumps(self.values))
+
+
+storage = LocaleStorage()
+cred = credentials.Certificate('src/creds.json')
 initialize_app(cred)
 
 
@@ -31,17 +51,14 @@ def push(token: str, title: str, body: str):
 
 @app.post('/token')
 async def save_user_device_token(info: UserInfoIn):
-    user_firebase_token[UUID(info.id)] = info.token
+    storage.write(str(info.id), info.token)
     return Response(status_code=204)
 
 
 @app.post('/')
-async def send_push_notification(info: PushInfoIn):
-    if token := user_firebase_token[UUID(info.user_id)]:
+async def send_push_notification_to_device(info: PushInfoIn):
+    if token := storage.read(str(info.user_id)):
         push(token, info.title, info.content)
         return Response(status_code=204)
 
     return Response(status_code=404)
-
-
-push('cwd_ZzbrStaFXi-eHfu0wp:APA91bFvyPXf-KKWIcYu2RoPu16EGVS8ApTHA5f20MlCsqHe9iAuPu1rLTaHFUzCX2eVqJCK_f3je42q37yF55tH-9aCsKKy5wMflUU2il74X2eeWLA7FcF_fMdnnzQu54_mm-Mg_ix3', 'Предупреждение', 'Тестовое оповещение')
